0d96beca5581261d892389558d83070992ce7c6d
Jefferson Souza <jeffersonsouza03@hotmail.com>
2024-10-10 19:40:27 -0300

    improve: refatorar navbar e navMobile

---
+++ docs/js/script.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";window.toggleNavMobile=()=>{const e=document.getElementById("navMobile");"block"===e.style.display?e.style.display="none":e.style.display="block"}})();
--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 import "./../css/style.css";
+
+window.toggleNavMobile = () => {
+  const navMobile = document.getElementById("navMobile");
+  navMobile.style.display === "block"
+    ? (navMobile.style.display = "none")
+    : (navMobile.style.display = "block");
+};
