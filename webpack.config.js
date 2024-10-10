0ed089a9d7bf862f36af2657aac27b04017e60ef
Jefferson Souza <jeffersonsouza03@hotmail.com>
2024-10-10 19:50:01 -0300

    fix: corrigir path das imagens do projeto

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./js/script.js",
@@ -55,6 +56,9 @@ module.exports = {
         removeStyleLinkTypeAttributes: true,
         useShortDoctype: true,
       },
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "img", to: "img" }],
     }),
   ],
   optimization: {
