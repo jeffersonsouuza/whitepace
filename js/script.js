995bc693a6b6e9a3858d2135d7090e4b84b2b21d
Jefferson Souza <jeffersonsouza03@hotmail.com>
2024-10-21 17:13:36 -0300

    improve: implementar controle do slider com swift

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ import "./../css/style.css";
 import SimpleAnime from "./modules/SimpleAnime.js";
 import SmoothScroll from "./modules/SmoothScroll.js";
 
+import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+// Navbar Mobile
 window.toggleNavMobile = () => {
   const navMobile = document.getElementById("navMobile");
   navMobile.style.display === "block"
@@ -9,9 +15,36 @@ window.toggleNavMobile = () => {
     : (navMobile.style.display = "block");
 };
 
+// Simple Anime Elements
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
 
+// Smooth Scroll
 const smoothScroll = new SmoothScroll(".nav", 0, 2000);
 smoothScroll.init();
+
+// Slider Testimonials
+new Swiper(".mySliderTestimonials", {
+  modules: [Pagination],
+  spaceBetween: 32,
+  freeMode: true,
+  pagination: {
+    el: ".slider-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1.5,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+    1152: {
+      slidesPerView: 3,
+    },
+  },
+});
